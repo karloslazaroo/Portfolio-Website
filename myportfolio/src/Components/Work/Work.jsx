00bb5fab40d859a58a7a8129c618b9a9a18a637f
karloslazaroo <101194055+karloslazaroo@users.noreply.github.com>
2023-07-18 15:10:20 +0800

    Update Work.jsx

--- myportfolio/src/Components/Work/Work.jsx
+++ myportfolio/src/Components/Work/Work.jsx
@@ -20,7 +20,7 @@ function Work() {
         <div className="work_item">
        
           <div className="img_div">
-            <Link to="askust"><img src={work1} ></img></Link>
+            <Link to="/askust"><img src={work1} ></img></Link>
            
           </div>
           <div className="details">
